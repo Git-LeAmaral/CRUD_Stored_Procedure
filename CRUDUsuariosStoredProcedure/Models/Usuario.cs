e6ed5671eada1d1578243e72ead20798966138f0
Leandro Amaral <lramaral02m@gmail.com>
2024-08-25 15:56:34 -0300

    Adicionar arquivos de projeto.

---
+++ CRUDUsuariosStoredProcedure/Models/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUDUsuariosStoredProcedure.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Digite o nome!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Digite o Sobrenome!")]
+        public string Sobrenome { get; set; }
+
+        [Required(ErrorMessage = "Digite o Email!"),
+        EmailAddress(ErrorMessage = "Email inválido!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Digite o Cargo!")]
+        public string Cargo { get; set; }
+    }
+}
